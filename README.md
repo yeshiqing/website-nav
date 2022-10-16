60e7a3aa07050654ca94a75e96fef382f1178e1d
jeffrey <908202572@qq.com>
2022-10-16 17:57:10 +0800

    logo with images

---
+++ README.md
@@ -0,0 +1,12 @@
+## 开发
+
+```shell
+yarn global add parcel-bundler
+parcel src/index.html
+```
+
+## build 命令
+
+```shell
+yarn build
+```
---
+++ src/main.js
@@ -0,0 +1,57 @@
+import imageURL_bilibili from './images/bilibili.png';
+let dataWebsite = localStorage.getItem("dataWebsite") || null
+let arrHashMap = JSON.parse(dataWebsite) || [
+    { logo: "A", logoType: "text", url: "https://www.acfun.cn", domain: "acfun.cn" },
+    { logo: imageURL_bilibili, logoType: "img", url: "https://www.bilibili.com", domain: "bilibili.com" }
+]
+
+let $lastLi = $('.siteList li:last')
+let render = () => {
+    arrHashMap.forEach((node) => {
+        let logo = ""
+        if (node.logoType === "img") {
+            logo = `<img src="${node.logo}" alt="${node.domain}">`
+        } else if (node.logoType === "text") {
+            logo = node.logo
+        }
+        let $li = $(`<li>
+                        <a href="${node.url}">
+                            <div class="site">
+                                <div class="logo">
+                                ${logo}
+                                </div>
+                                <div class="link">${node.domain}</div>
+                            </div>
+                        </a>
+                    </li>`)
+        $li.insertBefore($lastLi)
+    })
+}
+render()
+
+$('.addButton').on('click', (e) => {
+    let urlInput = window.prompt('请输入添加的站点网址：');
+    let urlReal = ""
+    if (urlInput.indexOf('http') === 0) {
+        urlReal = urlInput
+    } else {
+        urlReal = "https://" + urlInput
+    }
+
+
+    let reg = /[https|http]:\/\/(?:www\.)*([\w+|\.|\-]+)/gi;
+    let domain = reg.exec(urlReal)[1]
+    let logo = domain[0]
+    $(".siteList").find("li:not(:last)").remove()
+    arrHashMap.push({
+        logo: logo.toUpperCase(),
+        logoType: "text",
+        url: urlReal,
+        domain: domain
+    })
+    render()
+})
+
+window.onbeforeunload = () => {
+    localStorage.setItem("dataWebsite", JSON.stringify(arrHashMap))
+}
